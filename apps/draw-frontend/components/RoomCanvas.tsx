196e5db069dfa0e689c357cb49d636bd03555589
Ankit Raj <167684506+ctrlCraft01@users.noreply.github.com>
2026-06-13 09:00:49 +0530

    add some console log in frontend

--- apps/draw-frontend/components/RoomCanvas.tsx
+++ apps/draw-frontend/components/RoomCanvas.tsx
@@ -34,14 +34,24 @@ export default function RoomCanvas ({slug}:{slug:string}){
         const ws = new WebSocket(WS_URL)
 
         ws.onopen= ()=>{
+            console.log("ws open")
             setSocket(ws)
             ws.send(JSON.stringify({
                 type:"join_room",
                 roomId
             }))
         }
+        ws.onclose=(event)=>{
+            console.log("ws closed",event.code , event.reason)
+        }
+        ws.onerror=(event)=>{
+            console.log("ws error",event)
+        }
+        return ()=>{
+            console.log("cleanup closing socket")
+            ws.close()
 
-        return ()=>ws.close()
+        }
     },[roomId])
 
     if(roomId === null){
